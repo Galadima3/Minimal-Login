7ed2bbcf0fc010f12e92f65a4263011b8024bfee
Galadima3 <abrahamgaladima300@gmail.com>
2022-08-30 21:19:05 +0100

    Major Progress </>

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -13,100 +13,123 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
       backgroundColor: Colors.grey.shade300,
       body: SafeArea(
         child: Center(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 25,
-              ),
-              //logo
-
-              //Hello Again
-              Text(
-                'Hello Again! ',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                'Welcome back, you\'ve been missed! ',
-                style: TextStyle(fontSize: 20),
-              ),
-              SizedBox(
-                height: 25,
-              ),
-              //email textfield
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    border: Border.all(color: Colors.white),
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 5.0),
-                    child: TextField(
-                      decoration: InputDecoration(
-                          prefixIcon: Icon(Icons.email),
-                          hintText: 'Email',
-                          border: InputBorder.none),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //logo
+                Container(
+                    height: 250,
+                    child: Image.asset('images/flutter.png')),
+                //Hello Again
+                Text(
+                  'Hello Again! ',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 36),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Welcome back, you\'ve been missed! ',
+                  style: TextStyle(fontSize: 20),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                //email textfield
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 5.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.email),
+                            hintText: 'Email',
+                            border: InputBorder.none),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 25,
-              ),
-              //password
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    border: Border.all(color: Colors.white),
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 5.0),
-                    child: TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                          prefixIcon: Icon(Icons.password),
-                          hintText: 'Password',
-                          border: InputBorder.none),
+                SizedBox(
+                  height: 25,
+                ),
+                //password
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 5.0),
+                      child: TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.password),
+                            hintText: 'Password',
+                            border: InputBorder.none),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-
-              //padding: const EdgeInsets.symmetric(horizontal: 25.0),
-
-              //signin button
-              //TODO: Replace with MaterialButton
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  padding: EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                    color: Colors.deepPurple,
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Text(
-                    'Sign In',
-                    style: TextStyle(color: Colors.white, fontSize: 18 , fontWeight: FontWeight.bold),
-                  ),
+                SizedBox(
+                  height: 20,
                 ),
-              ),
 
-              //not a member? register button
-            ],
+                //padding: const EdgeInsets.symmetric(horizontal: 25.0),
+
+                //signin button
+                //TODO: Replace with MaterialButton
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      fixedSize: Size(300, 69),
+                      primary: Colors.deepPurple,
+                      //padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                      textStyle: const TextStyle(
+                          fontSize: 25, fontWeight: FontWeight.bold),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20))),
+                  onPressed: () {},
+                  child: Text('Sign In'),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text(
+                      'Not a member?',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'Register Now',
+                      style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue),
+                    )
+                  ],
+                )
+
+                //not a member? register button
+              ],
+            ),
           ),
         ),
       ),
